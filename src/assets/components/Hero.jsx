73142ca695a3d69384dc943ca7ac027eca22fca6
Azalor <khalidmohsen516@hotmail.com>
2025-10-05 04:19:19 +0300

    Update Hero.jsx

--- src/assets/components/Hero.jsx
+++ src/assets/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function hero() {
   return (
     <div className="hero-bg" id="home">
-        <h1 className="hero-header glitch">NEURAL NETWORK</h1>
+        <h1 className="hero-header home-header">NEURAL NETWORK</h1>
         <p className="hero-dis">Web Developer & Digital Architect</p>
     </div>
   )
